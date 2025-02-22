a4c932eeabc6ae3e10d9ea2738f5e5e101ebb428
jeetusingh247 <jeetusingh11010@gmail.com>
2025-02-22 16:41:22 +0530

    added config file

--- Code/intro_01.js
+++ Code/intro_01.js
@@ -1,18 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-
+// --> Day 01
 // console.log("i am jeetu singh");
 // let n: number = 5;
-// // num = "vinod"; // error // --> Day 01
+// // num = "vinod"; // error // 
 // n = 10;
 // // console.log(num1);
-
-
-// Day 02
+// --> Day 02
 // func to add two number 
 function sum(a, b) {
-    return a + b;     
+    return a + b;
 }
-
 // func call
 console.log(sum(10, 20)); // 30
