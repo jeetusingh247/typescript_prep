d33a59b29150e57376df5325c603849080346c83
jeetusingh247 <jeetusingh11010@gmail.com>
2025-02-22 16:20:27 +0530

    introduction to typescript

---
+++ Code/intro_01.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("i am jeetu singh");
+var n = 5;
+// num = "vinod"; // error
+n = 10;
+// console.log(num1);
---
+++ Code/intro_01.ts
@@ -0,0 +1,6 @@
+export {}; // to avoid global variable conflict
+console.log("i am jeetu singh");
+let n: number = 5;
+// num = "vinod"; // error
+n = 10;
+// console.log(num1);
