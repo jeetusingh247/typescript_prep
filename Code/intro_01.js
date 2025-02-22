9becbbe8114f008ccf7fb3bd7b6e3243c79acfd1
jeetusingh247 <jeetusingh11010@gmail.com>
2025-02-22 16:37:39 +0530

    modified for day02

--- Code/intro_01.js
+++ Code/intro_01.js
@@ -1,7 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("i am jeetu singh");
-var n = 5;
-// num = "vinod"; // error
-n = 10;
-// console.log(num1);
+
+// console.log("i am jeetu singh");
+// let n: number = 5;
+// // num = "vinod"; // error // --> Day 01
+// n = 10;
+// // console.log(num1);
+
+
+// Day 02
+// func to add two number 
+function sum(a, b) {
+    return a + b;     
+}
+
+// func call
+console.log(sum(10, 20)); // 30
--- Code/intro_01.ts
+++ Code/intro_01.ts
@@ -1,6 +1,16 @@
 export {}; // to avoid global variable conflict
-console.log("i am jeetu singh");
-let n: number = 5;
-// num = "vinod"; // error
-n = 10;
-// console.log(num1);
+
+// --> Day 01
+// console.log("i am jeetu singh");
+// let n: number = 5;
+// // num = "vinod"; // error // 
+// n = 10;
+// // console.log(num1);
+
+// --> Day 02
+// func to add two number 
+function sum(a: number, b: number): number {
+    return a + b;
+}
+// func call
+console.log(sum(10, 20)); // 30
